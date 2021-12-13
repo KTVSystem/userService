e513cee7aaa702294e03d3c254353ad3ac339b99
kris-sure <kukulyak.taras@gmail.com>
2021-12-13 22:53:22 +0200

    Finish filter functionality

--- server/src/api/repositories/user/user-repository.ts
+++ server/src/api/repositories/user/user-repository.ts
@@ -1,5 +1,6 @@
-import { User } from '../../interfaces/user/user';
 import { UserModel } from '../../models/user/user-model';
+import { findRoleByName } from './role-repository';
+import { perPage }  from '../../../config/settings';
 
 export const findUserByEmail = async (email: string) => {
     const user = (await UserModel.find({ email: email }).populate('role').limit(1))[0];
@@ -11,6 +12,44 @@ export const findUserByEmail = async (email: string) => {
 
 export const all = async () => {
     return UserModel.find({}).populate('role').populate('token');
+}
+
+export const allUsersCount = async (params) => {
+    const {email, role, status} = params;
+    let query = UserModel.find({});
+
+    if (typeof email !== 'undefined') {
+        query.find({email: { $regex: '.*' + email + '.*' } });
+    }
+    if (typeof role !== 'undefined') {
+        const roleObj  = await findRoleByName(role);
+        query.find({ role: roleObj._id });
+    }
+    if (typeof status !== 'undefined') {
+        query.find({ status: status });
+    }
+    return query.count();
+}
+
+export const allByQuery = async (params: any) => {
+    const {email, role, status} = params;
+
+    const page = (typeof params.page !== 'undefined') ? params.page : 1;
+    const perPageValue = Number(perPage);
+    const skip = (page === 1) ? 0 : (perPageValue * (page - 1));
+
+    let query = UserModel.find({});
+    if (typeof email !== 'undefined') {
+        query.find({email: { $regex: '.*' + email + '.*' } });
+    }
+    if (typeof role !== 'undefined') {
+        const roleObj  = await findRoleByName(role);
+        query.find({ role: roleObj._id });
+    }
+    if (typeof status !== 'undefined') {
+        query.find({ status: status });
+    }
+    return query.skip(skip).limit(perPageValue).populate('role').populate('token');
 }
 
 export const findUserById = async (id: string) => {
