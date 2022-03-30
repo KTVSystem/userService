c5e60bfdf33c75f1aaa410115ff949f3064e5a5d
tkuku911 <tkuku@softserveinc.com>
2022-03-30 20:36:40 +0300

    Start angular unit tests

--- client/src/app/login/login.component.spec.ts
+++ client/src/app/login/login.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +8,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [HttpClientModule],
     })
     .compileComponents();
   });
